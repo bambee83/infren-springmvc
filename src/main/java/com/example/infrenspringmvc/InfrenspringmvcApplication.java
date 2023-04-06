93ea555dea3a0c0ed7261850fa9db9eff530df47
bambee <daj0909@naver.com>
2023-04-07 00:58:53 +0900

    feat : "initial commit"

---
+++ src/main/java/com/example/infrenspringmvc/InfrenspringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.infrenspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfrenspringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfrenspringmvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/infrenspringmvc/InfrenspringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infrenspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrenspringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
